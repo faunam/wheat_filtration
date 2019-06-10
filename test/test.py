315a40cab9ed6fbec7d96233bd1db5b4fe7aa6bf
Fauna Mahootian <flm45@cornell.edu>
2019-06-10 14:12:09 -0400

    implemented split_corpus in munge.py and added tests. all tests pass. need to work on documentation and test format

--- test/test.py
+++ test/test.py
@@ -2,37 +2,72 @@ import munge
 import unittest
 
 
-def file_to_stringlist(filename):
-    pass
-
-
 def file_factory(n):
-    # makes a file with "the great big whale splashed the timid blue fox." repeated n times, no linebreaks
-    pass
+    # makes a file with "the great big whale splashed the timid blue fox.\n" repeated n times
+    with open("test_files/simple_whale_" + str(n) + ".txt", "w") as out:
+        for i in range(n):
+            out.write("the great big whale splashed the timid blue fox.\n")
+    # ... how to use this??
+
+
+def file_fact_2(n):
+  # makes a file with "the big white angel was on fire, and the black angel put
+  #  him out and said \"hey idiot\".\n" repeated n times
+    with open("test_files/simple_angel_" + str(n) + ".txt", "w") as out:
+        for i in range(n):
+            out.write(
+                "the big white angel was on fire, and the black angel put him out and said \"hey idiot\".\n")
 
 
 class TestMungeMethods(unittest.TestCase):
-    # def test_add(self):
-    #     self.assertEqual(munge.add_one(6), 7)
 
-    def test_split_corpus_from_file(self):
-        # want to check: joined split file is same as original
-        # split chunks are 100-500 words
-        # dont split mid sentence?
-        pass
+    def test_split_corpus(self):
+            # want to check: joined split file is same as original (? but i remove white space.. hmm)
+            # (all tokens are words, that may have punc connected. but no "" or white space)
 
-    def test_split_corpus_from_directory(self):
-        # want to check: joined split file is same as original
-        # split chunks are 100-500 words
-        # dont split mid sentence?
-        # dont miss any files in directory, dont pick up non text files
-        pass
+            # handles text files
+        for x in munge.split_corpus("test_files/simple_whale_100.txt"):
+          # all segments are more than 100, less than 500
+            self.assertTrue(len(x.split()) > 250)
+            self.assertTrue(len(x.split()) <= 502)
+          # all segments end on punctuation or are 500 words
+            self.assertTrue(
+                len(x.split()) == 500 or x[-1] == "." or x[-1] == "!" or x[-1] == "?")
+
+        for x in munge.split_corpus("test_files/quixote.txt"):
+          # all segments are more than 100, less than 500
+            self.assertTrue(len(x.split()) <= 500)
+            self.assertTrue(len(x.split()) > 250)
+          # all segments end on punctuation or are 500 words
+            self.assertTrue(
+                len(x.split()) == 500 or x[-1] == "." or x[-1] == "!" or x[-1] == "?")
+
+        # handles directories with text files and other kinds of files
+        for x in munge.split_corpus("test_files/"):
+          # all segments are more than 100, less than 500
+            self.assertTrue(len(x.split()) <= 500)
+            self.assertTrue(len(x.split()) > 250)
+          # all segments end on punctuation or are 500 words
+            self.assertTrue(
+                len(x.split()) == 500 or x[-1] == "." or x[-1] == "!" or x[-1] == "?")
+
+        # throws exn if source is not a txt file or directory ?? need to test exceptions?
+        # self.assertRaises(munge.split_corpus("dfd"),
+        #                   FileNotFoundError)
+        # self.assertRaises(munge.split_corpus("test_files/whale.txt"),
+        #                   FileNotFoundError)
+        # self.assertRaises(munge.split_corpus("test_files/fake.cry"),
+        #                   NotADirectoryError)
 
     def test_write_clean_corpus(self):
         # check:every line has correct formatting
         # ids are unique (i could raise an error if not)
         pass
 
+
+file_factory(100)
+file_factory(101)
+file_fact_2(50)
 
 if __name__ == '__main__':
     unittest.main()
--- wheat_filtration/munge.py
+++ wheat_filtration/munge.py
@@ -1,16 +1,64 @@
 from typing import List
+import os
+import subprocess
+import shlex
 
 
-def split_corpus_from_file(txt_file: str):
-        # splits corpus stored in txt_file into appropriately sized documents (100-500 words)
-        # returns a string list
-    pass
+def call(x): return subprocess.call(shlex.split(x))
 
 
-def split_corpus_from_directory(directory: str):
-    # splits corpus stored in directory into appropriately sized documents (100-500 words)
-    # returns a list of strings where each string is a document
-    pass
+def split_corpus(source: str):
+        # splits corpus stored in source into appropriately sized documents (100-500 words).
+        # source can be path to text file or directory containing multiple text files.
+        # if source is directory, must end in / or \ (whatever is appropriate to your system)!
+        # if source does not end in .txt, it is handled as a directory
+        # returns a string list.
+    full_corp = ""
+    if source[-4:] == ".txt":
+        # handles case where source is a text file
+        with open(source, "r") as inf:
+            full_corp += inf.read()
+
+    else:
+        # handles case where source is directory, or throws exception if it is not
+        directory = os.fsencode(source)
+        for thisfile in os.listdir(directory):
+            filename = os.fsdecode(thisfile)
+            if len(filename) < 4 or filename[-4:] != ".txt":
+                continue
+                # skips non text files
+            with open(source + filename, "r") as inf:
+                full_corp += inf.read()
+
+    tokens = full_corp.split()
+    num_tokens = len(tokens)
+    # tokens have punc attached
+    split_corp = []
+    i = 0
+    while i < (num_tokens-250):
+        j = next_punc_index(i+250, tokens)
+        new_seg = " ".join(tokens[i:j])
+        if j > (num_tokens - 250):  # end of string #not triggering for some reason
+            new_seg += " ".join(tokens[j:])
+        split_corp.append(new_seg)
+        i = j
+    # split_corp.append(" ".join(tokens[i:]))  # the remaining tokens
+
+    return split_corp
+
+
+def next_punc_index(i, tokens):
+    # returns the index of the element following the next punctuation after
+    # index i in an array of tokens. if no punctuation within 250 indeces after i,
+    # returns the index i + 250 (to ensure max 500 segment length)
+    curr_index = i
+    while curr_index < (i + 250):
+        possible_punc = tokens[curr_index][-1]
+        if possible_punc == "." or possible_punc == "!" or possible_punc == "?":
+            return curr_index + 1
+        else:
+            curr_index += 1
+    return curr_index
 
 
 def write_clean_corpus(split_corpus: List[str], doc_uniq_ids: list,
@@ -20,7 +68,3 @@ def write_clean_corpus(split_corpus: List[str], doc_uniq_ids: list,
     # <unique_id>\t<orig_doc_id>\t<text>
     # raises InvalidIDs if doc_uniq_ids has any repeats
     pass
-
-
-def add_one(n):
-    return n + 1
