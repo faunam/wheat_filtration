ee4ba8e9b7c1ae894ab75b3ec7e38bfe5a8f201d
Fauna Mahootian <flm45@cornell.edu>
2019-06-12 14:15:58 -0400

    changed small things

--- wheat_filtration/mallet.py
+++ wheat_filtration/mallet.py
@@ -1,14 +1,19 @@
-import os
-import subprocess
 import shlex
+import subprocess
+
 import numpy as np
 
 
-def call(x): return subprocess.call(shlex.split(x))
+MALLET_PATH = "/Users/fauma/Mallet-master/bin/mallet"  # None
+
+
+def call(x):
+    """executes string x as a command line prompt"""
+    return subprocess.call(shlex.split(x))
 
 
 def correct_format(filename):
-    # returns true if each line in file is of the format <unique_id>\t<orig_doc_id>\t<text>
+    """ returns true if each line in filename is of the format <unique_id>\t<orig_doc_id>\t<text>"""
     with open(filename, "r") as inf:
         for line in inf:
             if len(line.split("\t")) != 3:
@@ -17,12 +22,22 @@ def correct_format(filename):
 
 
 class TopicModel:
+
     def __init__(self, filepath, num_topics, optimize_interval):  # include opt interval?
-        # creates a mallet topic model of filepath, which is a txt file
-        # containing the preprocessed, formatted corpus. each line in the file
-        # should have the following format: <unique_id>\t<orig_doc_id>\t<text>
-        # outputs a topic_model object that has fields: topic_keys_20, doc_prop_matrix, doc_names, doc_ids
+        """Creates a mallet topic model of filepath, which is a txt file
+        containing the preprocessed, formatted corpus. each line in the file
+        should have the following format: <unique_id>\t<orig_doc_id>\t<text>
+        Outputs a topic_model object that has fields: topic_keys_20, doc_prop_matrix, doc_names, doc_ids,"""
         assert correct_format(filepath)
+
+        if MALLET_PATH is None:
+            try:
+                call("mallet")
+                path_to_mallet = "mallet"
+            except:
+                raise Exception(
+                    "Please enter the path to your Mallet directory in the MALLET_PATH variable at the top of mallet.py")
+
         num_docs = 0
         doc_ids = []
         doc_names = []
@@ -38,10 +53,9 @@ class TopicModel:
         filestem = filepath[:-4]  # remove .txt
         self.filestem = filestem
 
-        # how to call this file from mallet dir?
-        call(f"bin/mallet import-file --input " + filepath + " --output " +
+        call(path_to_mallet + " import-file - -input " + filepath + " - -output " +
              filestem + ".mallet --keep-sequence --remove-stopwords")
-        call(f"bin/mallet train-topics --input " + filestem + ".mallet --num-topics "
+        call(path_to_mallet + " train-topics --input " + filestem + ".mallet --num-topics "
              + str(num_topics) + " --output-topic-keys " +
              filestem + "_keys_20.txt --output-state "
              + filestem + "_state.gz --output-doc-topics " + filestem +
@@ -65,8 +79,8 @@ class TopicModel:
         self.state_path = filestem + "_state.gz"
 
     def get_n_topic_keys(self, n):
-      # returns a string list of the top n keywords for each topic
-        call("bin/mallet train-topics --input-state " + self.state_path +
+        """returns a string list of the top n keywords for each topic"""
+        call(MALLET_PATH + " train-topics --input-state " + self.state_path +
              " --input " + self.filestem + ".mallet --no-inference true --output-topic-keys "
              + self.filestem + "_keys_" + str(n) + ".txt --num-top-words " + str(n))
 
