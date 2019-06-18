14e267cf6b9912d12cc5231323e5aed1f9430a90
Fauna Mahootian <flm45@cornell.edu>
2019-06-18 11:03:31 -0400

    fixed corp to doc methods so that the max length of a doc is 500 words (in case the corpus is poetic or some other literary form that doesnt use a lot of punctuation). also fixed documentation for all methods.

--- wheat_filtration/munge.py
+++ wheat_filtration/munge.py
@@ -5,28 +5,16 @@ import string
 import nltk.data
 
 
-def _next_punc_index(i, tokens: List[str]):
-    """Returns the index of the element in an list of tokens following the next
-    punctuation after index i. If no punctuation is found within 250 indeces after i,
-    returns the index i + 250."""
-    curr_index = i
-    while curr_index < (i + 250):
-        possible_punc = tokens[curr_index][-1]
-        if possible_punc in (".", "!", "?"):
-            return curr_index + 1
-        curr_index += 1
-    return curr_index
-
-
 def import_corpus(source: str):
     """Load corpus from source into one string.
     Arguments:
-        source (str): the path to the txt file or directory (containing txt files)
+        source (str): the path to the text file or directory (containing text files)
             where the corpus is located. if source is a directory, it must end in /
-            or \\ (whatever is appropriate to your system)!
+            or \\ (whatever is appropriate to your system)! This function will parse all
+            files in the given directory that are not system files.
     Returns:
-        full_corp (str): a string containing the corpus
-        """
+        full_corp (str): a string containing the corpus"""
+
     full_corp = ""
     try:
         with open(source, "r") as in_file:
@@ -36,7 +24,8 @@ def import_corpus(source: str):
         for thisfile in os.listdir(directory):
             filename = os.fsdecode(thisfile)
             # skip system files
-            if filename[0] = ".":
+            # maybe add functionality that allows them to give a regex for the kinds of files they want to parse
+            if filename[0] == ".":  # eg or regex not in filename
                 continue
             try:
                 with open(source + filename, "r") as in_file:
@@ -47,13 +36,14 @@ def import_corpus(source: str):
 
 
 def corpus_to_documents(corpus: str):
-    """Splits corpus into appropriately sized documents (250+ words).
+    """Splits corpus into appropriately sized documents in the form of strings (250-500 words).
     Arguments:
         corpus (str): a string containing the corpus
     Returns:
-        corpus_documents (string list): a list containing strings representing documents in the corpus.
-        Documents are at least 250 words long and end on the next sentence after 250 words are reached, [better phrasing...]
-        unless the following is the last in the corpus, in which case it is also included in the document."""
+        corpus_documents (iterable of str): a list containing strings representing documents in the corpus.
+        Documents are 250-500 words, except possibly for the last document in the corpus. 
+        They end on the next sentence punctuation after 250 words, or at 500 words, whichever comes first.
+        If the sentence following a document is the last in the corpus, it is also included in the document."""
 
     sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
     sentences = sentence_detector.tokenize(
@@ -62,28 +52,35 @@ def corpus_to_documents(corpus: str):
     corpus_documents = []
     document_in_progress = []
     for sentence in sentences:
+        while len(document_in_progress) > 500:
+            corpus_documents.append(" ".join(document_in_progress[:500]))
+            document_in_progress = document_in_progress[500:]
         if len(document_in_progress) < 250:
             document_in_progress.extend(sentence.split())
         else:
-            # add document to corpus once it is over 250 words
+            # add document to corpus once it is between 250 and 500 words
             corpus_documents.append(" ".join(document_in_progress))
             document_in_progress = []
     # adds leftover at end of corpus to last document
-    corpus_documents[-1] = corpus_documents[-1] + \
-        " ".join(document_in_progress)
+    try:
+        corpus_documents[-1] = corpus_documents[-1] + \
+            " ".join(document_in_progress)
+    # allow user to import an abnormally small corpus; warn at time of topic model creation
+    except IndexError:
+        corpus_documents.append(document_in_progress)
 
     return corpus_documents
 
 
-# better name... maybe this function isnt useful to user, just helpful for gensim
 def corpus_to_doc_tokens(corpus: str):
-    """Splits corpus into tokenized documents of appropriate size (250+ words).
+    """Splits corpus into tokenized documents, in the form of lists of strings, of appropriate size (250-500 words).
     Arguments:
         corpus (str): a string containing the corpus
     Returns:
-        corpus_documents (iterable of iterable of str): an array of tokenized documents. 
-        Documents are at least 250 words long and end on the next sentence after 250 words are reached, [better phrasing...]
-        unless the following is the last in the corpus, in which case it is also included in the document. """
+        corpus_documents (iterable of iterable of str): a list of tokenized documents.
+        Documents are 250-500 words, except possibly for the last document in the corpus.
+        They end on the next sentence punctuation after 250 words, or at 500 words, whichever comes first.
+        If the sentence following a document is the last in the corpus, it is also included in the document."""
 
     sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
     sentences = sentence_detector.tokenize(
@@ -91,36 +88,44 @@ def corpus_to_doc_tokens(corpus: str):
 
     def clean_punc(token):
         # remove punctuation
+        # got this code snippet from stack overflow https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
         return token.translate(
             str.maketrans('', '', string.punctuation + "—"))
 
     corpus_documents = []
     document_in_progress = []
     for sentence in sentences:
+        while len(document_in_progress) > 500:
+            corpus_documents.append(" ".join(document_in_progress[:500]))
+            document_in_progress = document_in_progress[500:]
         if len(document_in_progress) < 250:
             document_in_progress.extend(clean_punc(sentence).split())
         else:
-            # add document to corpus once it is over 250 words
+            # add document to corpus once it is between 250 and 500 words
             corpus_documents.append(document_in_progress)
             document_in_progress = []
     # adds leftover at end of corpus to last document
-    corpus_documents[-1].extend(document_in_progress)
+    try:
+        corpus_documents[-1].extend(document_in_progress)
+    # allow user to import an abnormally small corpus; warn at time of topic model creation
+    except IndexError:
+        corpus_documents.append(document_in_progress)
 
     return corpus_documents
 
 
 def write_clean_corpus(split_corpus_list: List[str], doc_uniq_ids: list,
                        doc_names: List[str], new_file_name: str):
-    """Formats split corpus for topic modeling in Mallet, and removes punctuation.
+    """Formats corpus for topic modeling in Mallet, and removes punctuation.
     writes formatted corpus to file, where each document is formatted thusly:
     <unique_id>\t<orig_doc_id>\t<text>
-    new_file_name includes the filepath or will be created locally
+    new_file_name includes the filepath, or it will be created locally
     Arguments:
-        split_corpus_list (str list): list of all documents in corpus, 250-500
-            words each (use split_corpus).
+        split_corpus_list (iterable of str): list of all documents in corpus, 250-500
+            words each.
         doc_uniq_ids (list): list of document unique IDs. Must be the same length
             as split_corpus_list. IDs must be unique.
-        doc_names (str list): list of document names. Must be the same length
+        doc_names (iterable of str): list of document names. Must be the same length
             as split_corpus_list.
         new_file_name (str): path to the file where the prepped corpus will be stored.
     Raises:
@@ -139,6 +144,3 @@ def write_clean_corpus(split_corpus_list: List[str], doc_uniq_ids: list,
             # got this code snippet from stack overflow https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
             prepped_text = str_ids[i] + "\t" + doc_names[i] + "\t" + clean_line
             out.write(prepped_text + "\n")
-
-
-import_corpus("test/")
