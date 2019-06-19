92eaca494e352900b8cbb4f3f25f286a6278d942
Fauna Mahootian <flm45@cornell.edu>
2019-06-19 13:49:50 -0400

    started filter and keywords

---
+++ wheat_filtration/filter.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def topic_vocab_counts():
+    """Returns a matrix of word counts for each topic for the entire vocabulary.
+    Arguments:
+
+    Returns:
+      (ndarray(n_topics, n_docs)): each entry is the total word count for that word in that topic
+    """
+    pass
+
+
+def filter_corp(relevant_topics: list, key_words: list, super_key_words: list):
+    # filters corpus by percentage of rel_topics, key_words, and super_key_words
+    # returns subcorpus
+    pass
